9f303764974b2ac43a1710e8f4cdd2039de23417
William Sørensen <williambilly42@gmail.com>
2023-09-16 14:56:19 +0200

    devenv init

---
+++ devenv.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.git
+    pkgs.ocamlPackages.ppx_inline_test
+    pkgs.opam
+  ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/languages/
+  languages.java.enable = true;
+  languages.ocaml = {
+    enable = true;
+
+    # packages = [ "ocaml-lsp-server" ];
+  };
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}

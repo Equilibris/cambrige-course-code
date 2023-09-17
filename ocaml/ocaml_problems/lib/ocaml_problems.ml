9cca59c2937ff9640e00588b75ce61c93d757c25
William Sørensen <williambilly42@gmail.com>
2023-09-17 23:20:31 +0200

    solve more problems

--- ocaml/ocaml_problems/lib/ocaml_problems.ml
+++ ocaml/ocaml_problems/lib/ocaml_problems.ml
@@ -1,3 +1,13 @@
+module Utils = struct
+    let rec rep v c = if c = 0 then [] else v::(c-1 |> rep v)
+
+    let join sep = function
+        | [] -> ""
+        | h :: t -> List.fold_left (fun a -> fun b -> a ^ sep ^ b) h t
+
+    let print_ls ls = ls |> join " ; " |> print_endline
+end
+
 module Fact = struct
     let rec fact n = if n = 1 then 1 else n * fact (n - 1)
 
@@ -143,15 +153,16 @@ end
 
 module Decode = struct
     open EncodeShare
+    open Utils
 
     let decode ls = ls 
         |> (List.map (
             function
-                | Many (count, value) -> let rec rep v c = if c = 0 then [] else v::(c-1 |> rep v) in rep value count
+                | Many (count, value) -> rep value count
                 | One value -> [value]
             )
         )
-        |> (List.fold_left (fun a -> fun b -> List.concat [a ; b]) [])
+        |> (List.fold_left List.append [])
 
     let%test _ = decode [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d"; Many (4, "e")] = ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];;
     let%test _ = decode [] = [];;
@@ -175,9 +186,125 @@ module EncodeDirect = struct
     let%test _ = encode [] = [];;
 end
 
-module Replicate = struct
-    let duplicate ls = ls |> (List.concat_map (fun v -> [ v ; v ]))
+module Duplicate = struct
+    let duplicate = List.concat_map (fun v -> [ v ; v ])
 
     let%test _ = duplicate ["a"; "b"; "c"; "c"; "d"] = ["a"; "a"; "b"; "b"; "c"; "c"; "c"; "c"; "d"; "d"];;
     let%test _ = duplicate [] = [];;
 end
+
+module Replicate = struct
+    open Utils
+
+    let replicate count = List.concat_map (fun v -> rep v count)
+
+    let%test _ = replicate 2 ["a"; "b"; "c"; "c"; "d"]  = ["a"; "a"; "b"; "b"; "c"; "c"; "c"; "c"; "d"; "d"];;
+    let%test _ = replicate 10 [] = [];;
+end
+
+module DropN = struct
+    let drop n ls = let rec aux curr = function
+        | [] -> []
+        | h :: t -> if curr = 1 then aux n t else h :: (aux (curr - 1) t)
+    in aux n ls
+
+    (* let%test_unit _ = drop 3 ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] |> List.fold_left (fun a -> fun b -> a ^ b) "" |> print_endline;; *)
+    let%test _ = drop 3 ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] = ["a"; "b"; "d"; "e"; "g"; "h"; "j"];;
+    let%test _ = drop 1 ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] = [];;
+    let%test _ = drop 3 [] = [];;
+end
+
+module Split = struct
+    let split ls count = let rec aux fst ls count = match ls with
+        | [] -> (List.rev fst, ls)
+        | h :: t -> if count = 0 then (List.rev fst, ls) else count - 1 |> aux (h :: fst) t
+    in aux [] ls count
+
+    let%test _ = split ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 3 = (["a"; "b"; "c"], ["d"; "e"; "f"; "g"; "h"; "i"; "j"]);;
+    let%test _ = split ["a"; "b"; "c"; "d"] 5 = (["a"; "b"; "c"; "d"], []);;
+    let%test _ = split [] 5 = ([], []);;
+end
+
+module Slice = struct
+    let rec slice ls start stop = match ls with
+        | [] -> []
+        | h :: t -> if 
+            start != 0
+        then slice t (start - 1) (stop - 1)
+        else if stop = -1 then [] (* A bit cursed but thats life *)
+        else h :: (slice t 0 (stop - 1))
+
+    (* let%test_unit _ = slice ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 2 6 |> List.fold_left (fun a -> fun b -> a ^ b) "" |> print_endline;;  *)
+    let%test _ = slice ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 2 6 = ["c"; "d"; "e"; "f"; "g"];;
+    let%test _ = slice ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 2 2 = ["c"];;
+end
+
+module Rot = struct
+    let rotate = let rec aux fst ls count = match ls with 
+        | [] -> List.append ls (List.rev fst)
+        | h :: t -> if
+            count = 0
+        then List.append ls (List.rev fst)
+        else aux (h :: fst) t (count - 1)
+    in aux []
+
+    (* let%test_unit _ = rotate ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"] 3 |> List.fold_left (fun a -> fun b -> a ^ b) "" |> print_endline;;  *)
+    let%test _ = rotate ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"] 3 = ["d"; "e"; "f"; "g"; "h"; "a"; "b"; "c"];
+end
+
+module Remove = struct
+    let remove_at = let rec aux fst idx = function
+        | [] -> fst
+        | h :: t -> if idx = 0
+        then List.append fst t
+        else aux (h :: fst) (idx - 1) t
+    in aux []
+
+    let%test _ = remove_at 1 ["a"; "b"; "c"; "d"] = ["a"; "c"; "d"];;
+end
+
+module Insert = struct
+    let insert_at = let rec aux fst v idx = function
+        | [ ] -> List.rev (v :: fst)
+        | h :: t -> if idx = 1 then List.append (List.rev(v :: h :: fst)) t
+                               else aux (h :: fst) v (idx - 1) t
+    in aux []
+
+    (* let%test_unit _ = insert_at "alfa" 1 ["a"; "b"; "c"; "d"] |> print_ls ;; *)
+    let%test _ = insert_at "alfa" 1 ["a"; "b"; "c"; "d"] = ["a"; "alfa"; "b"; "c"; "d"];;
+    let%test _ = insert_at "alfa" 10 ["a"; "b"; "c"; "d"] = ["a"; "b"; "c"; "d"; "alfa"];;
+end
+
+module Range = struct
+    let range' n = let rec aux = function
+        | 0 -> []
+        | v -> (v - 1) :: (aux (v - 1))
+    in aux n |> List.rev
+
+    let range f t = t - f + 1 |> range' |> List.map ((+) f)
+
+    let%test _ = range 4 9 = [4; 5; 6; 7; 8; 9];;
+end
+
+module Combos = struct
+    let rec combos n ls = if n = 0 then [[]] else if
+        n = 1
+    then List.map (fun a -> [a]) ls
+    else if List.length ls = n then [ls]
+    else match ls with 
+        | [] -> [] (* TODO: no idea what to do here *)
+        | h :: t -> List.append (List.map (fun v -> h :: v) (combos (n - 1) t)) (combos n t)
+
+    let%test _ = combos 2 ["a"; "b"; "c"; "d"] = [["a"; "b"]; ["a"; "c"]; ["a"; "d"]; ["b"; "c"]; ["b"; "d"]; ["c"; "d"]];;
+end
+
+module Disjoint = struct
+    open Split
+
+    let rec multipart ls = function
+        | [] -> []
+        | h :: t -> let (a, b) = (split ls h) in a :: (multipart b t)
+
+    let%test _ = multipart [1 ; 2 ; 3 ; 4] [1 ; 2 ; 1] = [[1] ; [2 ; 3] ; [4]];;
+end
+

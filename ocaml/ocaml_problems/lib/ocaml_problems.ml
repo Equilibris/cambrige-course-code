953c3dde32c71218efda38a8b885e4b5c6f4cf36
William Sørensen <williambilly42@gmail.com>
2023-09-24 14:55:47 +0200

    solve more problems

--- ocaml/ocaml_problems/lib/ocaml_problems.ml
+++ ocaml/ocaml_problems/lib/ocaml_problems.ml
@@ -1,13 +1,27 @@
-module Utils = struct
-    let rec rep v c = if c = 0 then [] else v::(c-1 |> rep v)
+(* exts *)
+module Int = struct
+    include Int
+    let rec pow a b = if b = 0 then 1 else if b = 1 then a else (if b mod 2 = 1 then a else 1) * (pow (a * a) (b / 2))
+end
+module List = struct
+    include List
 
-    let join sep = function
-        | [] -> ""
-        | h :: t -> List.fold_left (fun a -> fun b -> a ^ sep ^ b) h t
-
-    let print_ls ls = ls |> join " ; " |> print_endline
+    let pmap f a b = let inner a b = List.map (fun v -> f a v) b in List.concat_map (fun v -> inner v b) a
 end
 
+(* utils *)
+let rec rep v c = if c = 0 then [] else v::(c-1 |> rep v)
+
+let join sep = function
+    | [] -> ""
+    | h :: t -> List.fold_left (fun a -> fun b -> a ^ sep ^ b) h t
+
+let print_ls ls = ls |> join " ; " |> (fun v -> "[" ^ v ^ "]") |> print_endline
+
+
+exception ArbitraryFail;;
+
+(* exs *)
 module Fact = struct
     let rec fact n = if n = 1 then 1 else n * fact (n - 1)
 
@@ -153,7 +167,6 @@ end
 
 module Decode = struct
     open EncodeShare
-    open Utils
 
     let decode ls = ls 
         |> (List.map (
@@ -194,7 +207,6 @@ module Duplicate = struct
 end
 
 module Replicate = struct
-    open Utils
 
     let replicate count = List.concat_map (fun v -> rep v count)
 
@@ -300,11 +312,225 @@ end
 
 module Disjoint = struct
     open Split
+    open Combos
 
     let rec multipart ls = function
         | [] -> []
         | h :: t -> let (a, b) = (split ls h) in a :: (multipart b t)
 
+        (* Cant be bothered to test this  *)
+    let group ls sets = combos (List.fold_left (+) 0 sets) ls |> List.map (fun v -> multipart v sets)
+
     let%test _ = multipart [1 ; 2 ; 3 ; 4] [1 ; 2 ; 1] = [[1] ; [2 ; 3] ; [4]];;
+    (* let%test _ = group ["a"; "b"; "c"; "d"] [2; 1] =  *)
+    (*     [[["a"; "b"]; ["c"]]; [["a"; "c"]; ["b"]]; [["b"; "c"]; ["a"]]; *)
+    (*      [["a"; "b"]; ["d"]]; [["a"; "c"]; ["d"]]; [["b"; "c"]; ["d"]]; *)
+    (*      [["a"; "d"]; ["b"]]; [["b"; "d"]; ["a"]]; [["a"; "d"]; ["c"]]; *)
+    (*      [["b"; "d"]; ["c"]]; [["c"; "d"]; ["a"]]; [["c"; "d"]; ["b"]]];; *)
 end
 
+module IsPrime = struct
+    let is_prime n = if n < 2 then false else let rec aux k = if k * k > n then true else if n mod k = 0 then false else aux (k + 1) in aux 2
+
+    let%test _ = is_prime 1 |> not;;
+    let%test _ = is_prime 7;;
+    let%test _ = is_prime 12 |> not;;
+    let%test _ = is_prime 997;;
+    (* let%test _ = is_prime 2305843009213693951;; *)
+end
+
+module Gcd = struct
+    let rec gcd a b = let
+        rem = a mod b
+    in if rem = 0 then b else gcd b rem
+
+    let%test _ = gcd 129512 9121312 = 8;;
+    let%test _ = gcd 3 2 = 1;;
+    let%test _ = gcd 6 3 = 3;;
+    let%test _ = gcd 6 2 = 2;;
+end
+
+module CoPrime = struct
+    open Gcd
+
+    let co_prime a b = gcd a b |> ((=) 1)
+
+    let%test _ = co_prime 3 2;;
+end
+
+module Totient = struct
+    open Range
+    open CoPrime
+
+    let phi = function
+        | 1 -> 1
+        | n -> n - 1 |> range 1 |> List.filter (co_prime n) |> List.length
+
+    let%test _ = phi 10 = 4;;
+end
+
+module Factors = struct
+    let rec factors n = if n = 1 then [] else 
+        let rec aux k = if k > n then k :: (factors (n / k)) else if n mod k = 0 then (k :: (factors (n / k))) else k + 1 |> aux
+        in aux 2
+
+    let%test _ = factors 315 = [3; 3; 5; 7];;
+end
+
+module Factors'= struct
+    open Encode
+    open Factors
+
+    let factors' n = n |> factors |> encode |> List.map (fun (a, b) -> (b,a))
+    
+    (* let%test_unit _ = factors2 315 |> List.map (fun (a, b) -> (Int.to_string a) ^ "," ^ (Int.to_string b)) |> join ";" |> print_endline;; *)
+    let%test _ = factors' 315  =[(3, 2); (5, 1); (7, 1)];;
+end
+
+module Totient2 = struct 
+    open Factors'
+
+    let phi' n = factors' n |> List.map (fun (p, m) -> (p - 1) * (Int.pow p (m - 1))) |> List.fold_left ( * ) 1
+
+    (* let%test_unit _ = ipow 2 61 |> Int.to_string |> print_endline;; *)
+    let%test _ = phi' 10 = 4;;
+    let%test _ = phi' 13 = 12;;
+end
+
+(* module T1v2 = struct *)
+(*     open Totient *)
+(*     open Totient2 *)
+
+(*     let%test_unit _ = phi 10 |> timeit |> print_endline;; *)
+(*     let%test_unit _ = phi' 10 |> timeit |> print_endline;; *)
+(* end *)
+
+module Goldbach = struct
+    open IsPrime
+
+    let goldbach n = let rec aux k = if k < n && is_prime k then if is_prime (n - k) then (k, (n - k)) else (aux (k + 1)) else (aux (k + 1)) in aux 2
+
+    let%test _ = goldbach 28 = (5, 23);;
+    (* let%test _ = range 10 20 |> List.filter (fun a -> a |> is_prime |> not) |> List.map (fun v -> (v, goldbach v)) =  *)
+    (*     [(10, (3, 7)); (12, (5, 7)); (14, (3, 11)); (16, (3, 13)); (18, (5, 13)); (20, (3, 17))];; *)
+end
+
+module Logic = struct
+    open Range
+
+    type bool_expr =
+      | Var of string
+      | Not of bool_expr
+      | And of bool_expr * bool_expr
+      | Or of bool_expr * bool_expr;;
+
+    let only_bit n = Int.shift_left 1 n
+
+    (* let bool_perm n = n |> ((+) 1) |> only_bit |> range' |> List.map (fun k -> range' n |> List.map (fun a -> Int.logand k (only_bit a)));; *)
+    let bool_perm n = n |> ((+) 1) |> only_bit |> range' |> List.map (fun k -> range' n |> List.map (only_bit) |> List.map (Int.logand k) |> List.map ((!=) 0));;
+
+    (* let%test_unit _ = bool_perm 2 |> List.map (List.map Bool.to_string) |> List.map (fun v -> "(" ^ (join ", " v) ^")") |> print_ls;; *)
+
+    let zip a = List.map2 (fun a -> fun b -> (a, b)) a
+
+    let snd (_, v) = v
+
+    let table vars expr = let
+        internal = List.length vars |> bool_perm |> List.map (zip vars)
+    in let rec aux t = function
+        | Var name -> List.find (fun (n, _) -> n = name) t |> snd
+        | Not v -> aux t v |> not
+        | And (a, b) -> (aux t a) && (aux t b)
+        | Or (a, b) -> (aux t a) || (aux t b)
+    in List.map (fun t -> (t, aux t expr)) internal
+
+    (* let%test _ = List.rev (table ["a"; "b"] (And (Var "a", Or (Var "a", Var "b")))) = [([("a", true); ("b", true)], true); ([("a", true); ("b", false)], true); ([("a", false); ("b", true)], false); ([("a", false); ("b", false)], false)];; *)
+    (* let%test_unit _ = table ["a"; "b"] (And (Var "a", Or (Var "a", Var "b"))) |> List.map (fun (ls, v) -> "[" ^ (List.map (fun(a,b) -> "(\""^a^"\","^(Bool.to_string b)^")") ls |> join "; ") ^ "], " ^ (Bool.to_string v)) |> print_ls;; *)
+end
+
+module Gray = struct
+    let rec gray = function
+        | 0 -> [""]
+        | n -> let sub = n - 1 |> gray in List.append (List.map (fun v -> "0" ^ v) sub) (List.rev sub |> List.map (fun v -> "1" ^ v))
+
+    let%test _ = gray 1 = ["0"; "1"];;
+    let%test _ = gray 2 = ["00"; "01"; "11"; "10"];;
+    let%test _ = gray 3 = ["000"; "001"; "011"; "010"; "110"; "111"; "101"; "100"];;
+end
+
+module Huffman = struct
+    type 'a tree = 
+        | Leaf of int * 'a
+        | Branch of ('a tree) * ('a tree);;
+
+    let rec cost = function
+        | Leaf (c, _) -> c
+        | Branch (a, b) -> (cost a) + (cost b)
+
+    let rec to_string = function
+        | Leaf (_, v) -> "Leaf " ^ v
+        | Branch (a, b) -> "Branch(" ^ (to_string a) ^ ", " ^ (to_string b) ^ ")";;
+
+
+    let enter = List.map (fun (a, b) -> Leaf(b, a))
+    let sort_tree v = List.sort (fun l -> fun r -> (cost l) - (cost r)) v;;
+    let rec build_tree = function
+        | []  -> raise ArbitraryFail
+        | [ x ] -> x
+        | a :: b :: tail -> (Branch (a, b) :: tail) |> sort_tree |> build_tree
+
+    let rec calculate_prefix = function
+        | Leaf (_, v) -> [v, ""]
+        | Branch (a, b) -> List.append (calculate_prefix a |> List.map (fun (value, prefix) -> (value, "0" ^ prefix))) (calculate_prefix b |> List.map (fun (value, prefix) -> (value, "1" ^ prefix)));;
+
+    let compute v = enter v |> sort_tree |> build_tree |> calculate_prefix;;
+
+    let freqs = [("a", 45); ("b", 13); ("c", 12); ("d", 16); ("e", 9); ("f", 5)];;
+
+    (* let%test_unit _ = compute freqs |> List.map (fun (a,b) -> a ^ b) |> print_ls;; *)
+    let%test _ = compute freqs = [("a", "0"); ("c", "100"); ("b", "101"); ("f", "1100"); ("e", "1101"); ("d", "111")];;
+end
+
+module BinaryTree = struct
+    type 'a binary_tree =
+      | Empty
+      | Node of 'a * 'a binary_tree * 'a binary_tree;;
+end
+
+module AllBalancedTrees = struct
+    open BinaryTree
+
+    let rec cbal_tree = function
+        | 0 -> [Empty]
+        | n -> ...
+
+    let%test _ = cbal_tree 4 = [
+        Node ('x',
+            Node ('x', Empty, Empty), 
+            Node ('x',
+                Node ('x', Empty, Empty),
+                Empty)
+            );
+        Node ('x',
+            Node ('x', Empty, Empty),
+            Node ('x',
+                Empty,
+                Node ('x', Empty, Empty)
+            )
+        );
+        Node ('x',
+            Node ('x',
+                Node ('x', Empty, Empty),
+                Empty
+            ),
+            Node ('x', Empty, Empty)
+        );
+        Node ('x',
+            Node ('x',
+                Empty,
+                Node ('x', Empty, Empty)
+            ),
+            Node ('x', Empty, Empty)
+        )
+    ];;
+end
